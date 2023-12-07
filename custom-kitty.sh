f442ecc4ec6519530d3ba5476f4a47cd34701786
R1ck <38839363+void4121@users.noreply.github.com>
2023-12-07 09:15:19 -0500

    Update custom-kitty.sh

--- custom-kitty.sh
+++ custom-kitty.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+#install kitty segment
 cd
-sudo apt install kitty git zsh -y
-wait
-
-cd /.config/kitty
+sudo apt install kitty git zsh curl -y
+cd .config/kitty
 
 git clone --depth 1 https://github.com/dexpota/kitty-themes.git ~/.config/kitty/kitty-themes
-ln -s ./kitty-themes/themes/Floraverse.conf ~/.config/kitty/theme.conf
+ln -s ./kitty-themes/themes/ayu_mirage.conf ~/.config/kitty/theme.conf
+touch kitty.conf && echo "include ./theme.conf" > kitty.conf
 
-touch kitty.conf < echo "include ./theme.conf"
+
+#custom zsh shell
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+Y
 
 exit
