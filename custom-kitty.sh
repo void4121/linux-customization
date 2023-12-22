859e0d94d09996f8cd70e9d7bfadd1cedb9b6dc9
R1ck <38839363+void4121@users.noreply.github.com>
2023-12-21 23:45:50 -0500

    Update custom-kitty.sh

--- custom-kitty.sh
+++ custom-kitty.sh
@@ -14,8 +14,6 @@ touch kitty.conf && echo "include ./theme.conf" > kitty.conf
 #custom zsh shell
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
-
-
-cp  ~/.zshrc .zshrc-backup
-cp .zshrc ~/
+cd
+cp  .zshrc .zshrc-backup
 exit
