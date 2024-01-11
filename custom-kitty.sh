e5aeecaac03b27bf6a24838f72d93c5ecdc10a17
R1ck <38839363+void4121@users.noreply.github.com>
2024-01-11 15:13:59 -0500

    Update custom-kitty.sh

--- custom-kitty.sh
+++ custom-kitty.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 
-#install kitty segment
-cd
-sudo pacman -S kitty git zsh curl
-cd .config/kitty
+#software installation
+
+sudo pacman -Sy kitty git zsh curl hyprpaper
+cd ~/.config/kitty
 
 git clone --depth 1 https://github.com/dexpota/kitty-themes.git ~/.config/kitty/kitty-themes
 #create soft link to theme configuration
 ln -s ./kitty-themes/themes/ayu_mirage.conf ~/.config/kitty/theme.conf
 touch kitty.conf && echo "include ./theme.conf" > kitty.conf
-
+echo "background_opacity 0.95" >> ~/.config/kitty/kitty.conf
 
 #custom zsh shell
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" & sleep 5
 
-cd
-cp .zshrc .zshrc-backup
+cp ~/.zshrc ~/.zshrc-backup
 cp ~/linux-customization/.zshrc ~/
-
-cd
-echo "background_opacity 0.95" >> .config/kitty/kitty.conf
 
 mkdir ~/wallpaper
 cp -f ~/linux-customization/wallpaper/* ~/wallpaper
