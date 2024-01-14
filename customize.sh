20704a5c2ddee5e0f4985b82835f1c7c35eb6319
rick <ramirez.r1214@gmail.com.com>
2024-01-14 00:21:13 -0500

    update install v2

--- customize.sh
+++ customize.sh
@@ -2,7 +2,7 @@
 
 #software installation
 
-sudo pacman -Sy kitty git zsh curl hyprpaper otf-font-awesome waybar neovim net-tools neofetch git rofi pavucontrol
+sudo pacman -Sy kitty git zsh curl hyprpaper otf-font-awesome waybar neovim net-tools neofetch git rofi pavucontrol arandr
 
 ### kitty setup
 cd ~/.config/kitty
