6d26519f17986d520ed100b1ee29c9a9827168f3
rick <ramirez.r1214@gmail.com>
2024-02-16 14:11:53 -0500

    update apps2

--- apps.sh
+++ apps.sh
@@ -6,7 +6,7 @@ pacman -S
 
 
 yay -S os-prober visual-studio-code-bin grimblast-git network-manager network-manager-applet spotify discord
-yay -S onedrive webcord thunar nwg-look libreoffice
+yay -S onedrive webcord thunar nwg-look libreoffice gimp
 
 #customization
 yay Candy icons nordic-darker
