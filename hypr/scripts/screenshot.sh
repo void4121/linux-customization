290ddce7ab8721fec9b0623cc65b338acb4ff11d
Rick <ramirez.r1214@gmail.com>
2024-02-20 10:15:54 -0500

    fixed bug in screenshot script

--- hypr/scripts/screenshot.sh
+++ hypr/scripts/screenshot.sh
@@ -1,12 +1,18 @@
-in/bash
+#!/bin/bash
 
 # Set the base name for the screenshot files
 base_name="screenshot"
 
+# Set the directory to store the screenshots
+screenshot_dir="$HOME/screenshots"
+
+# Create the directory if it doesn't exist
+mkdir -p "$screenshot_dir"
+
 # Find the highest existing screenshot number
 highest_number=0
-for file in "${base_name}"*.png; do
-    number=$(echo "$file" | sed "s/${base_name}\([0-9]\+\)\.png/\1/")
+for file in "${screenshot_dir}/${base_name}"*.png; do
+    number=$(echo "$file" | sed "s|${screenshot_dir}/${base_name}\([0-9]\+\)\.png|\1|")
     if [[ $number -gt $highest_number ]]; then
         highest_number=$number
     fi
@@ -14,7 +20,7 @@ done
 
 # Increment the highest number and create the next screenshot file
 next_number=$((highest_number + 1))
-next_file="${base_name}${next_number}.png"
-grimblast save area ~/screenshots/"$next_file"
+next_file="${screenshot_dir}/${base_name}${next_number}.png"
+grimblast save area "$next_file"
 
-echo "Created ~/screenshots/$next_file"
+echo "Created $next_file"
